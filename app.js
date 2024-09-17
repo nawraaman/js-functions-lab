69645fd84cd95e369b801c6c5ad4c6c613e1ea87
nawraaman <nawra.aman55@gmail.com>
2024-09-17 20:00:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function greeting(myName, lastName) {
+  console.log(`Hi ${myName} ${lastName}`)
+}
+greeting('Nawra', 'Aman')
+
+function multi(num1, num2) {
+  result = num1 * num2
+  console.log(result)
+}
+multi(2, 3)
+
+function computeArea(width, height) {
+  rectangle = width * height
+  console.log(
+    `The area of a rectangle with a width of ${width} and a height of ${height} is ${rectangle} saquare units`
+  )
+}
+computeArea(4, 4)
